ce638bb45fe3e43aeda257b7b0ef3597b5995d5b
Bluedillydilly <dylancuprewich@gmail.com>
2020-03-14 19:19:11 -0400

    init commit

---
+++ genre-adder.js
@@ -0,0 +1,10 @@
+// find where music description stuff located
+// find genre and descriptors associated with the song
+// to textnode othe genre and descriptors
+// ???
+// profit
+
+
+let genre = '';
+let styles = [];
+let descriptors = [];
